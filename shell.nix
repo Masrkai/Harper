94d0f727afcfcc8e6437514814803344cbe449c7
Masrkai <118424417+Masrkai@users.noreply.github.com>
2026-07-18 04:45:02 +0300

    moved state.sh to shell.nix

--- shell.nix
+++ shell.nix
@@ -20,6 +20,31 @@ pkgs.mkShell {
     alias review='[ -f target/llvm-cov/html/index.html ] && xdg-open target/llvm-cov/html/index.html || { echo "No report found, run test first"; }'
 
     alias package-test-remote='nix-build -E "with import <nixpkgs> {}; callPackage ./default.nix {}"'
-    alias package-test-local='nix-build -E "with import <nixpkgs> {}; callPackage ./default-local.nix {}"'
+    alias package-test-local='nix-build -E "with import <nixpkgs> {}; callPackage ./default-local.nix {}'
+
+    alias state='
+      echo "=== 1. Routing table ==="
+      ip route show
+      echo ""
+      echo "=== 2. ARP cache ==="
+      ip neigh show
+      echo ""
+      echo "=== 3. tc state ==="
+      echo "== Generally"
+      tc qdisc show
+      echo "-- eth0 --"
+      tc qdisc show dev eth0
+      echo "-- ifb0 --"
+      tc qdisc show dev ifb0
+      echo ""
+      echo "=== 4. Kernel parameters ==="
+      echo "ip_forward:"
+      cat /proc/sys/net/ipv4/ip_forward
+      echo "rp_filter:"
+      cat /proc/sys/net/ipv4/conf/all/rp_filter
+      echo ""
+      echo "=== 5. rpfilter chain ==="
+      sudo nft list chain inet nixos-fw rpfilter
+    '
   '';
 }
